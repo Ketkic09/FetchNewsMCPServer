8077ae8bbd49c3cf127c5dc2c8c3a94750e313b3
Ketkic09 <ketki.chavan22@gmail.com>
2025-05-05 02:13:38 +0530

    complete code

--- index.js
+++ index.js
@@ -1,53 +1,46 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js"
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from 'zod';
-// import mcp.server from FastMCP
 
-const APIKEY = process.env.APIKEY
+const APIKEY = process.env.API_KEY
 
 const server = new McpServer({
-    name: 'TechCrunch News Fetcher',
+    name: 'News Fetcher',
     version: '1.0.0',
-
 })
-
-// async function getNewsByTopic(topic) {
-//     if (topic.toLowerCase() === "computers") {
-//         return "computers searched"
-//     }    
-//     if (topic.toLowerCase() === "finance") {
-//         return "finance searched"
-//     }
-//     // return { code: 404, content: "Something went wrong" }
-//     // return { code: 200, content: "success" }
-//     return "misc"
-// }
-
-// server.tool('getTechCrunchData', {
-//     topic: z.string()
-// }, async ({ topic }) => {
-//     return { content: [{ type: "text", text: JSON.stringify(await getNewsByTopic(topic)) }] }
-// })
 
 server.tool(
     "fetchNews",
     { topic: z.string() },
     async ({ topic }) => {
-      const response = await fetch(`https://newsapi.org/v2/everything?q=${topic}&apiKey=${APIKEY}`);
-      const data = JSON.stringify(response.articles[0]['title']);
+      const response = await fetch(`https://newsapi.org/v2/everything?q=${topic}&apiKey=${APIKEY}`,{
+        headers:{
+          "Content-Type": "application/json"
+        }
+      });
+
+      if (!response.ok) {
+
+        return {
+          content: [{ type: "text", text: "Error fetching news" }]
+        };
+      }
+      const data = await response.json();
+
+      const titles = data.articles.map(article => {
+        return article.title
+      })
+      const titleText = titles.join("\n")
       return {
-        content: [{ type: "text", text: data }]
-      };
+        content: [{ type: "text", text: JSON.stringify(titleText) }]
+      }
     }
 );
-
 
 function addNo(a,b){
     const sum = a+b
     return sum
 }
-
-
 
 server.tool('addTwoNos', {
     a: z.number(),
@@ -59,16 +52,9 @@ server.tool('addTwoNos', {
 )
 
 
-// server.start({
-//       transportType: "stdio",
-// });
-
 async function init() {
     const transport = new StdioServerTransport();
-    // console.log("transport created", transport)
     const serverResponse = JSON.stringify(await server.connect(transport));
-    console.log(serverResponse)
-    // console.log("Server connected")
 }
 
 init()
